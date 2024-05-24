c6f25b84cf7548ed73fc37e5a51797ce13ee2001
Gaëtan <oss@gaetans.dev>
2024-05-24 13:46:42 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+<p align="center">
+<img src="https://raw.githubusercontent.com/felangel/mason/master/assets/mason_full.png" height="125" alt="mason logo" />
+</p>
+
+<p align="center">
+<a href="https://github.com/felangel/mason/actions"><img src="https://github.com/felangel/mason/workflows/mason/badge.svg" alt="mason"></a>
+<a href="https://pub.dev/packages/very_good_analysis"><img src="https://img.shields.io/badge/style-very_good_analysis-B22C89.svg" alt="style: very good analysis"></a>
+<a href="https://opensource.org/licenses/MIT"><img src="https://img.shields.io/badge/license-MIT-purple.svg" alt="License: MIT"></a>
+</p>
+
+<p align="center">
+A collection of bricks that enable developers to be more productive when writing code.
+</p>
+
+<em><p align="center">This project is merely a fork of [Luke Moody's mason_bricks](https://github.com/LukeMoody01/mason_bricks) repository with changes to the bricks and the documentation.</p></em>
+
+---
+
+## Bricks 🧱
+
+| Brick | Description | Version |
+| ----- | ----------- | ------- |
+
+| [feature_brick](https://brickhub.dev/bricks/feature_brick) | A brick to create a feature using best practices and your state management of choice! | 0.1.0 |
+
+## Documentation
+
+- [Feature Brick](https://github.com/gaetschwartz/mason_bricks/tree/master/bricks/feature_brick)
+
+<!-- ## Contributing
+
+Please read [CONTRIBUTING.md](https://github.com/gaetschwartz/mason_bricks/tree/master/CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests to us. -->
+
+## Maintainers
+
+- [Gaëtan Schwartz](https://github.com/gaetschwartz)
---
+++ bricks/feature_brick/__brick__/{{feature_name.snakeCase()}}/view/{{feature_name.snakeCase()}}_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';{{#isBloc}}
+import 'package:{{{fullPath}}}/bloc/bloc.dart';{{/isBloc}}{{#isCubit}}
+import 'package:{{{fullPath}}}/cubit/cubit.dart';{{/isCubit}}{{#isProvider}}
+import 'package:{{{fullPath}}}/provider/provider.dart';{{/isProvider}}
+import 'package:{{{fullPath}}}/widgets/{{feature_name.snakeCase()}}_body.dart';
+
+/// {@template {{feature_name.snakeCase()}}_page}
+/// A description for {{feature_name.pascalCase()}}Page
+/// {@endtemplate}
+class {{feature_name.pascalCase()}}Page extends StatelessWidget {
+  /// {@macro {{feature_name.snakeCase()}}_page}
+  const {{feature_name.pascalCase()}}Page({super.key});
+
+  /// The static route for {{feature_name.pascalCase()}}Page
+  static Route<dynamic> route() {
+    return MaterialPageRoute<dynamic>(builder: (_) => const {{feature_name.pascalCase()}}Page());
+  }
+
+  /// The route name for {{feature_name.pascalCase()}}Page
+  static const String routeName = '/{{feature_name.snakeCase()}}';
+
+{{#isBloc}}
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => {{feature_name.pascalCase()}}Bloc(),
+      child: const Scaffold(
+        body: {{feature_name.pascalCase()}}View(),
+      ),
+    );
+  } {{/isBloc}}{{#isCubit}}
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => {{feature_name.pascalCase()}}Cubit(),
+      child: const Scaffold(
+        body: {{feature_name.pascalCase()}}View(),
+      ),
+    );
+  } {{/isCubit}} {{#isProvider}}
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => {{feature_name.pascalCase()}}Notifier(),
+      child: const Scaffold(
+        body: {{feature_name.pascalCase()}}View(),
+      ),
+    );
+  } {{/isProvider}} {{#isRiverpod}}
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: {{feature_name.pascalCase()}}View(),
+    );
+  } {{/isRiverpod}} {{#isNone}}
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: {{feature_name.pascalCase()}}View(),
+    );
+  } {{/isNone}}
+}
+
+/// {@template {{feature_name.snakeCase()}}_view}
+/// Displays the Body of {{feature_name.pascalCase()}}View
+/// {@endtemplate}
+class {{feature_name.pascalCase()}}View extends StatelessWidget {
+  /// {@macro {{feature_name.snakeCase()}}_view}
+  const {{feature_name.pascalCase()}}View({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const {{feature_name.pascalCase()}}Body();
+  }
+}
